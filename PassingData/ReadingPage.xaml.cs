3dd46854c633b0658f66a1c1258f87879327d4e5
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2021-06-22 10:54:29 +1000

    Add extra authority selection with newline.

--- PassingData/ReadingPage.xaml.cs
+++ PassingData/ReadingPage.xaml.cs
@@ -66,7 +66,7 @@ namespace PassingData
 			{
 				if (authority.Selected)
 				{
-					selectedAuthorities += authority.TagLabel + ", ";
+					selectedAuthorities += authority.TagLabel + "\n";
 				}
 			}
 
